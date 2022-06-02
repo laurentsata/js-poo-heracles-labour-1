7cfe642c9d5edea58b4a429e5446b0a8c742c728
john Doe <sataward666@gmail.com>
2022-06-02 13:30:59 +0200

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+import Fighter from "./src/Fighter.js";
+
 // First Labour : Heracles vs Nemean Lion
+const heracles = new Fighter("💀 Heracles", 20, 6);
+const nemeanLion = new Fighter("🦁 Lion", 11, 13);
 // use your Figher class here
+console.log(heracles);
+console.log(nemeanLion);
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,11 @@
 /* Fighter class definition */
+const MAX_LIFE = 100
+export default class Fighter {
+    constructor(name, strength, dexterity, life) {
+        this.name = name;
+        this.strength = strength; 
+        this.dexterity = dexterity;
+        this.life = life = MAX_LIFE;
+        }
+    }
+
